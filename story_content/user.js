a32ce674e9ae81a60008212eacd0a890cf2b1ce9
romeoefimov <refimov@edventure.ru>
2022-04-07 12:36:20 +0300

    1

---
+++ story_content/user.js
@@ -0,0 +1,201 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6MyBsCWK8pQ":
+        Script1();
+        break;
+      case "6AIy1UoWFt7":
+        Script2();
+        break;
+      case "6Kqh2KM82db":
+        Script3();
+        break;
+      case "5iZeSHzwZuz":
+        Script4();
+        break;
+      case "6Fmk93NGPpv":
+        Script5();
+        break;
+      case "6J0XpBUuDKp":
+        Script6();
+        break;
+      case "6KqbIaHhMdN":
+        Script7();
+        break;
+      case "6Rmc5pHTvQr":
+        Script8();
+        break;
+      case "62uNP9HBo7j":
+        Script9();
+        break;
+      case "6EhJZBkk0d1":
+        Script10();
+        break;
+      case "5xh3CvCg2Gl":
+        Script11();
+        break;
+      case "5fL0WtI35fZ":
+        Script12();
+        break;
+      case "5jOqV7SmNXr":
+        Script13();
+        break;
+      case "6UZvLXodbl3":
+        Script14();
+        break;
+  }
+}
+
+function Script1()
+{
+  if(void 0===window.stencilfullscreen){window.stencilfullscreen=!0;var player=GetPlayer(),xhttp=new XMLHttpRequest,targlink="https://cluelabs.com/stencil/display/widget-full-screen-display?chart=MzIyfDE3ODB8MWE3MTlmOTlmMmMxOWEyMzExYTE1YTBmNzFmMGI0ZTY";xhttp.onreadystatechange=function(){4==this.readyState&&200==this.status&&""!=this.responseText&&(eval(this.responseText),player.SetVar("stencilrendered",parseInt(player.GetVar("stencilrendered"))+1))},xhttp.open("GET",targlink,!0),xhttp.send()}
+}
+
+function Script2()
+{
+  FullscreenMode('player');
+}
+
+function Script3()
+{
+  FullscreenMode('player');
+}
+
+function Script4()
+{
+  FullscreenMode('player');
+}
+
+function Script5()
+{
+  FullscreenMode('player');
+}
+
+function Script6()
+{
+  FullscreenMode('player');
+}
+
+function Script7()
+{
+  FullscreenMode('player');
+}
+
+function Script8()
+{
+  FullscreenMode('player');
+}
+
+function Script9()
+{
+  FullscreenMode('player');
+}
+
+function Script10()
+{
+  FullscreenMode('player');
+}
+
+function Script11()
+{
+  FullscreenMode('player');
+}
+
+function Script12()
+{
+  FullscreenMode('player');
+}
+
+function Script13()
+{
+  FullscreenMode('player');
+}
+
+function Script14()
+{
+  var num1 = 0
+var num2 = 0
+var num3 = 0
+var num4 = 0
+var num5 = 0
+var num6 = 0
+var num7 = 0
+var num8 = 0
+var num9 = 0
+var num10 = 0
+var num11 = 0
+var num12 = 0
+var num13 = 0
+var num14 = 0
+var num15 = 0
+var num16 = 0
+var num17 = 0
+var num18 = 0
+var num19 = 0
+var num20 = 0
+var num21 = 0
+var num22 = 0
+var num23 = 0
+var num24 = 0
+var num25 = 0
+var sum1 = 0
+var sum2 = 0
+var sum3 = 0
+var sum4 = 0
+var sum5 = 0
+var sum6 = 0
+
+var player = GetPlayer();
+
+num1 = player.GetVar("q11");
+num2 = player.GetVar("q12");
+num3 = player.GetVar("q13");
+num4 = player.GetVar("q14");
+num5 = player.GetVar("q15");
+num6 = player.GetVar("q21");
+num7 = player.GetVar("q22");
+num8 = player.GetVar("q23");
+num9 = player.GetVar("q24");
+num10 = player.GetVar("q25");
+num11 = player.GetVar("q31");
+num12 = player.GetVar("q32");
+num13 = player.GetVar("q33");
+num14 = player.GetVar("q34");
+num15 = player.GetVar("q35");
+num16 = player.GetVar("q41");
+num17 = player.GetVar("q42");
+num18 = player.GetVar("q43");
+num19 = player.GetVar("q44");
+num20 = player.GetVar("q45");
+num21 = player.GetVar("q51");
+num22 = player.GetVar("q52");
+num23 = player.GetVar("q53");
+num24 = player.GetVar("q54");
+num25 = player.GetVar("q55");
+
+sum1 = num1+num2+num3+num4+num5
+
+sum2 = num6+num7+num8+num9+num10
+
+sum3 = num11+num12+num13+num14+num15
+
+sum4 = num16+num17+num18+num19+num20
+
+sum5 = num21+num22+num23+num24+num25
+
+sum6 = sum1+sum2+sum3+sum4+sum5
+
+player.SetVar("total1",sum1)
+
+player.SetVar("total2",sum2)
+
+player.SetVar("total3",sum3)
+
+player.SetVar("total4",sum4)
+
+player.SetVar("total5",sum5)
+
+player.SetVar("intotal",sum6)
+}
+
